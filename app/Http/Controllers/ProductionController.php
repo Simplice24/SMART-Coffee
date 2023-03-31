287574493554568a1bb48d227708880d3f35de24
Simplice NIYONZIMA <89002414+Simplice24@users.noreply.github.com>
2023-03-31 05:39:37 -0800

    controller

--- app/Http/Controllers/ProductionController.php
+++ app/Http/Controllers/ProductionController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Production;
+use App\Models\Sales;
 
 class ProductionController extends Controller
 {
--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Production;
+use App\Models\Sales;
 
 class SalesController extends Controller
 {
