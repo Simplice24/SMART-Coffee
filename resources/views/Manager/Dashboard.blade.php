0c39cc3e52bdf96f08a22add07c635f0d14ed08e
Simplice NIYONZIMA <89002414+Simplice24@users.noreply.github.com>
2023-04-04 01:01:40 -0800

    livewire

--- resources/views/Manager/Dashboard.blade.php
+++ resources/views/Manager/Dashboard.blade.php
@@ -178,7 +178,7 @@
                 <a href="" style="text-decoration:none; color:white;">
                   <div class="card-body">
                     <img src="/Customized/assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
-                    <h4 class="font-weight-normal mb-3">Weekly Sales <i class="mdi mdi-chart-line mdi-24px float-right"></i>
+                    <h4 class="font-weight-normal mb-3">Monthly Sales <i class="mdi mdi-chart-line mdi-24px float-right"></i>
                     <h1 class="mb-5"></h1>
                   </div>
                  </a> 
