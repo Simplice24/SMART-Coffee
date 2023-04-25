481fa695c4faad38441a2147c5b74784608b7c49
Simplice NIYONZIMA <89002414+Simplice24@users.noreply.github.com>
2023-04-25 04:26:28 -0800

    Chart

---
+++ database/migrations/2023_04_25_110915_create_reporters_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reporters', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('farmer_id');
+            $table->string('username');
+            $table->string('password');
+            $table->timestamps();
+
+            $table->foreign('farmer_id')->references('id')->on('farmers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reporters');
+    }
+};
