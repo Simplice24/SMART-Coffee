1aa923871ef55709841896a79c535e63bdff0157
Simplice NIYONZIMA <89002414+Simplice24@users.noreply.github.com>
2023-04-27 00:53:27 -0800

    Profile success  buttons

--- resources/views/Manager/Manager-profile.blade.php
+++ resources/views/Manager/Manager-profile.blade.php
@@ -195,7 +195,7 @@
                     </div>
                 </div>
                 <div class="mt-5 text-center">
-                  <button class="btn btn-primary profile-button" type="submit">Save Profile</button>
+                  <button class="btn btn-success profile-button" type="submit">Save Profile</button>
                 </div>
                 </form>
             </div>
@@ -236,7 +236,7 @@
                     </div>
                 </div>
                 <div class="mt-5 text-center">
-                <button class="btn btn-primary profile-button" type="submit">Change password</button>
+                <button class="btn btn-success profile-button" type="submit">Change password</button>
                 </div>
                 </form>
             </div>
--- resources/views/Official/Official-profile.blade.php
+++ resources/views/Official/Official-profile.blade.php
@@ -208,7 +208,7 @@
                     </div>
                 </div>
                 <div class="mt-5 text-center">
-                  <button class="btn btn-primary profile-button" type="submit">Save Profile</button>
+                  <button class="btn btn-success profile-button" type="submit">Save Profile</button>
                 </div>
                 </form>
             </div>
@@ -249,7 +249,7 @@
                     </div>
                 </div>
                 <div class="mt-5 text-center">
-                <button class="btn btn-primary profile-button" type="submit">Change password</button>
+                <button class="btn btn-success profile-button" type="submit">Change password</button>
                 </div>
                 </form>
             </div>
